6c04aa79820f6590024a74a3a25bc1ab58e0567d
Shahin Azad <hi@shahinism.com>
2023-03-09 21:29:59 +0100

    extract aliases to its own module.

---
+++ home-manager/aliases.nix
@@ -0,0 +1,13 @@
+{
+  home.shellAliases = {
+    c = "xclip -selection clipboard";
+    hm = "home-manager";
+    man = "batman";
+    watch = "batwatch";
+    cat = "bat";
+    grep = "batgrep";
+    ls = "exa";
+    mkdir = "mkdir -pv";
+    top = "btm";
+  };
+}
