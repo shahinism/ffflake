221ed2e54533519bea5953120e018791a0e606cb
Shahin Azad <hi@shahinism.com>
2023-03-10 18:38:19 +0100

    add back missing ssh over gpg configuration.

--- home-manager/bash/init.sh
+++ home-manager/bash/init.sh
@@ -1,2 +1,7 @@
+### Variables ###
+# SSH using GPG
+gpg-connect-agent /bye
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+
 export EDITOR="emacsclient -nw"
 export VISUAL="emacsclient -nw"
