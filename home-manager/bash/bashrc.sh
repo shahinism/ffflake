4e237b5f7137e5cf63ebb0c1f5894d2855379477
Shahin Azad <hi@shahinism.com>
2023-03-10 18:28:40 +0100

    feat (bash): Add personal functions.

--- home-manager/bash/bashrc.sh
+++ home-manager/bash/bashrc.sh
@@ -99,3 +99,31 @@ shopt -s cdable_vars
 
 # Show the status of stopped/running jobs before exit
 shopt -s checkjobs
+
+### Functions ###
+docker-clean () {
+    docker volume rm $(docuer volume ls -qf dangling=true) || echo "No volume to clean!"
+    docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }') || echo "No network to clean!"
+    # see: http://stackoverflow.com/questions/32723111/how-to-remove-old-and-unused-docker-images
+    docker rmi $(docker images --filter "dangling=true" -q --no-trunc) || echo "No dangling images to clean!"
+    docker rm $(docker ps -qa --no-trunc --filter "status=exited") || echo "No exited container to delete!"
+}
+
+nix-clean () {
+    # https://discourse.nixos.org/t/what-to-do-with-a-full-boot-partition/2049/2
+    nix-env --delete-generations old
+    nix-store --gc
+    nix-channel --update
+    nix-env -u --always
+    for link in /nix/var/nix/gcroots/auto/*
+    do
+        rm $(readlink "$link")
+    done
+    nix-collect-garbage -d
+
+    # FIXME home-manager gets removed with this operation.
+    nix-shell '<home-manager>' -A install
+}
+
+# https://docs.gitignore.io/install/command-line
+function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@; }
