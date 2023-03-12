b81f4e06cebf3ce0bb1c2dc936e270db734bad33
Shahin Azad <hi@shahinism.com>
2023-03-12 11:16:23 +0100

    Update qtile symlink config so it doesn't need to run through Nix.

--- home-manager/qtile.nix
+++ home-manager/qtile.nix
@@ -1,5 +1,8 @@
+{config, ...}:
 {
-  xdg.configFile.qtile = {
-    source = ./qtile;
+  home.file = {
+    ".config/qtile" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/projects/personal/ffflake/home-manager/qtile/";
+    };
   };
 }
