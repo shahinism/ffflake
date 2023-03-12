4558b31de307db5598573f65e185f821be782015
Shahin Azad <hi@shahinism.com>
2023-03-12 10:35:31 +0100

    add emacs gui desktop entry.

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -1,11 +1,47 @@
 {pkgs, config, ...}:
 
-{
+let
+  emacsClientGuiDesktop = pkgs.makeDesktopItem {
+    desktopName = "Emacs Client GUI";
+    name = "Emacs Client GUI";
+    genericName = "Text Editor";
+    comment = "Edit Text";
+    mimeTypes = [
+      "text/english"
+      "text/plain"
+      "text/x-makefile"
+      "text/x-c++hdr""text/x-c++src"
+      "text/x-chdr"
+      "text/x-csrc"
+      "text/x-java"
+      "text/x-moc"
+      "text/x-pascal"
+      "text/x-tcl"
+      "text/x-tex"
+      "application/x-shellscript"
+      "text/x-c"
+      "text/x-c++"
+    ];
+    exec = "emacsclient -c -a \"emacs\" %F";
+    icon="emacs";
+    terminal = false;
+    categories = [
+      "Development"
+      "TextEditor"
+      "Utility"
+    ];
+    startupWMClass="Emacs";
+  };
+in {
+  home.packages = [
+    emacsClientGuiDesktop
+  ];
+
   programs.emacs = {
     enable = true;
     package = pkgs.emacs29;
   };
-  
+
   services.emacs = {
     enable = true;
     package = pkgs.emacs29;
