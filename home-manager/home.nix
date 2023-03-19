159b80089c9fab3c559de5615645c5af8fe909bc
Shahin Azad <hi@shahinism.com>
2023-03-19 20:39:30 +0100

    feat (desktop): Set default applications.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -146,4 +146,57 @@ in {
   services.syncthing = {
     enable = true;
   };  
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+      "audio/x-ms-asx" = "vlc.desktop";
+      "audio/x-ms-wma" = "vlc.desktop";
+      "audio/mp2" = "vlc.desktop";
+      "audio/x-mpegurl" = "vlc.desktop";
+      "audio/ogg" = "vlc.desktop";
+      "audio/x-scpls" = "vlc.desktop";
+      "audio/mpeg" = "vlc.desktop";
+      "audio/x-wav" = "vlc.desktop";
+      "audio/aac" = "vlc.desktop";
+      "audio/mp4" = "vlc.desktop";
+      "audio/vnd.rn-realaudio" = "vlc.desktop";
+      "text/x-c++src" = "emacsclient.desktop";
+      "text/x-pascal" = "emacsclient.desktop";
+      "text/x-google-video-pointer" = "emacsclient.desktop";
+      "text/x-c++hdr" = "emacsclient.desktop";
+      "text/html" = "emacsclient.desktop";
+      "text/plain" = "emacsclient.desktop";
+      "text/tcl" = "emacsclient.desktop";
+      "text/x-csrc" = "emacsclient.desktop";
+      "text/x-makefile" = "emacsclient.desktop";
+      "text/x-chdr" = "emacsclient.desktop";
+      "text/x-tex" = "emacsclient.desktop";
+      "text/x-java" = "emacsclient.desktop";
+      "text/x-moc" = "emacsclient.desktop";
+      "video/3gpp" = "vlc.desktop";
+      "video/ogg" = "vlc.desktop";
+      "video/quicktime" = "vlc.desktop";
+      "video/x-ms-wmv" = "vlc.desktop";
+      "video/vnd.rn-realvideo" = "vlc.desktop";
+      "video/mp4" = "vlc.desktop";
+      "video/mpeg" = "vlc.desktop";
+      "video/x-matroska" = "vlc.desktop";
+      "video/x-flic" = "vlc.desktop";
+      "video/x-msvideo" = "vlc.desktop";
+      "video/x-theora+ogg" = "vlc.desktop";
+      "video/x-flv" = "vlc.desktop";
+      "application/x-xpinstall" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/xml" = "firefox.desktop";
+      "application/json" = "firefox.desktop";
+    };
+  };
 }
