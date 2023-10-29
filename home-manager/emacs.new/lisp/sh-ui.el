0e53b4b8575fdd4df583f8d8e5b6bbafe3d313af
Shahin Azad <hi@shahinism.com>
2023-10-29 15:50:08 +0100

    refactor(emacs.new): To doom-zenburn and lambda line.

--- home-manager/emacs.new/lisp/sh-ui.el
+++ home-manager/emacs.new/lisp/sh-ui.el
@@ -6,6 +6,11 @@
 
 (leaf nano
   :url "https://github.com/rougier/nano-emacs"
+  :disabled t         ;; NOTE it looks beautiful, but too limiting on
+                      ;; using a custom theme. For now, given the
+                      ;; readability of the code, I prefer to use
+                      ;; custom themes, and load only the modules I
+                      ;; like.
   :straight (nano :host github :repo "rougier/nano-emacs")
   :pre-setq
   ;; (nano-font-family-monospaced . "Fira Code")
@@ -40,6 +45,47 @@
   (nano-theme)
   (nano-faces)
   (setq nano-theme-var "light")
+  )
+
+
+(leaf doom-themes
+  :url "https://github.com/doomemacs/themes"
+  :ensure t
+  :custom
+  (doom-themes-enable-bold . t)
+  (doom-themes-enable-italic . t)
+  :config
+  (load-theme 'doom-zenburn t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config)
+  )
+
+(leaf all-the-icons
+  :url "https://github.com/domtronn/all-the-icons.el"
+  :ensure t
+  :if (display-graphic-p))
+
+(leaf lambda-line
+  :url "https://github.com/Lambda-Emacs/lambda-line"
+  :straight (lambda-line :host github :repo "Lambda-Emacs/lambda-line")
+  :custom
+  (lambda-line-position . 'top)                            ;; Set position of status-line
+  (lambda-line-abbrev . t)                                 ;; abbreviate major modes
+  (lambda-line-hspace . "  ")                              ;; add some cushion
+  (lambda-line-prefix . t)                                 ;; use a prefix symbol
+  (lambda-line-prefix-padding . nil)                       ;; no extra space for prefix
+  (lambda-line-status-invert . nil)                        ;; no invert colors
+  (lambda-line-gui-ro-symbol . " ⨂")                      ;; symbols
+  (lambda-line-gui-mod-symbol . " ⬤")
+  (lambda-line-gui-rw-symbol . " ◯")
+  (lambda-line-space-top . +.50)                           ;; padding on top and bottom of line
+  (lambda-line-space-bottom . -.50)
+  (lambda-line-symbol-position . 0.1)                      ;; adjust the vertical placement of symbol
+  :config
+  (lambda-line-mode)
+  (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_")))
   )
 
 ;; Set font
